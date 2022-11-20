7e79869fcafca2c1319ddf8ee33e7a28c09bec63
Mrunalikale21 <113428448+Mrunalikale21@users.noreply.github.com>
2022-11-20 19:35:07 +0530

    Add files via upload

---
+++ Mrunali/evenodd.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    cout<<"enter number"<<endl;
+    int n;
+    cin>>n;
+
+    if(n%2==0){
+        cout<<"even"<<endl;
+    }
+    else{
+        cout<<"odd"<<endl;
+        
+    }
+
+    return 0;     
+}
